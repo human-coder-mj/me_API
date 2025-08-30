2886f8bae67717744b4d150f21b92bd475cd1b55
human-coder-mj <jhamanas4532@gmail.com>
2025-08-30 12:55:17 +0530

    created the profile model

--- apps/profile_api/models.py
+++ apps/profile_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Main profile model containing personal information
+    """
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self)->str:
+        return str(self.name)
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
