715af9c911da826ed138ff7f819e85a533eaf45e
human-coder-mj <jhamanas4532@gmail.com>
2025-09-02 17:54:17 +0530

    updated the urls for education

--- apps/education_api/urls.py
+++ apps/education_api/urls.py
@@ -5,16 +5,8 @@ app_name = 'education_api'
 
 urlpatterns = [
     # Basic Education CRUD operations
-    path('education/', views.EducationListCreateView.as_view(), name='education-list-create'),
-    path('education/<int:pk>/', views.EducationDetailView.as_view(), name='education-detail'),
-    
     # Education by profile
-    path('education/profile/<int:profile_id>/', views.education_by_profile, name='education-by-profile'),
-    path('education/profile/<str:name>/', views.education_by_profile_name, name='education-by-profile-name'),
-    
-    # Reference data endpoints
-    path('education/institutions/', views.education_institutions, name='education-institutions'),
-    path('education/degrees/', views.education_degrees, name='education-degrees'),
+    path('profile/<str:name>/education/', views.education_by_profile_name, name='education-by-profile-name'),
     
     # Search functionality
     path('education/search/', views.education_search, name='education-search'),
